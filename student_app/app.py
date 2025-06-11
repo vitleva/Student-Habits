375556b3442215945b6ffe1d397d3b01b59ab49d
Gundareva Angela <gundarevaangela9@gmail.com>
2025-06-12 00:15:38 +0700

    фикс нез какой

--- student_app/app.py
+++ student_app/app.py
@@ -76,7 +76,8 @@ def history():
     return render_template("history.html", history=history)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)  
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
 
 
 
